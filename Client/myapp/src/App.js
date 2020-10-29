4219091a534988d57cb12a2e719f4c36c9769394
Alonso <pikachualonso@gmail.com>
2020-10-29 01:22:22 -0400

    fix frontend

--- Client/myapp/src/App.js
+++ Client/myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Veronica and Alonso
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alonso and Veronica
         </a>
       </header>
     </div>
